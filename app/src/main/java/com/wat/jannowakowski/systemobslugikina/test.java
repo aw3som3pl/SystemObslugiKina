cd242502e68ffe834ae3bf3fb59393fd02996524
Jan Nowakowski <aw3som3pl@gmail.com>
2018-11-21 20:09:45 +0100

    test2

--- app/src/main/java/com/wat/jannowakowski/systemobslugikina/test.java
+++ app/src/main/java/com/wat/jannowakowski/systemobslugikina/test.java
@@ -1,8 +1,6 @@
 package com.wat.jannowakowski.systemobslugikina;
 
-/**
- * Created by Jan Nowakowski on 21.11.2018.
- */
-
 public class test {
+    private String abcd;
+    private String bdbd;
 }
